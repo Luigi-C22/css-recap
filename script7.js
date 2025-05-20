6335d26d99b6eaf9633a78c748cc291422b61d29
Luigi Ciangola <lc71@libero.it>
2025-05-20 20:54:06 +0200

    aggiorno lo script

---
+++ script7.js
@@ -0,0 +1,18 @@
+//calculator
+
+// DEFINIZIONE ALGORITMO:
+// 1. Ingredienti (input)
+
+// 2. Qual è il nostro output?
+
+// Elementi utili dal DOM:
+// Cosa devo catturare?
+
+// Listeners
+// Quali fra gli elementi definiti sopra devono possederli?
+
+// Funzioni
+// Quali funzioni sono strettamente necessarie?
+
+// Bonus
+// E se volessimo implementare le operazioni a catena? 
