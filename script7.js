77443bbd165a4306ff3e169a2263f0e330197cff
Luigi Ciangola <lc71@libero.it>
2025-05-21 19:08:07 +0200

    aggiunte istruzioni

--- script7.js
+++ script7.js
@@ -3,16 +3,30 @@
 // DEFINIZIONE ALGORITMO:
 // 1. Ingredienti (input)
 // Segno matematico, valore: +, -, *, /, 0, ..., 9
+
 // 2. Qual è il nostro output?
 //  "il risultato"   
+
+//Aspetti di acquisizione dei dati dall'utente:
 // Elementi utili dal DOM:
 // Cosa devo catturare?
+// Display, Segni di operazione, cifre da 0...9
 
 // Listeners
 // Quali fra gli elementi definiti sopra devono possederli?
+// Su tutti i bottoni con cifre, segni di operazione, AC ed uguale 
 
 // Funzioni
 // Quali funzioni sono strettamente necessarie?
+// Sicuramente i tasti AC e = dovranno scaturire una funzione!
 
 // Bonus
 // E se volessimo implementare le operazioni a catena? 
+
+let display = document.getElementById("display"); // Elemnto singolo HTM
+let operatorEls = document.getElementsByClassName("operator"); // Cosa sarà salvato qau dentro? Array!
+let digitEls = document.getElementsByClassName("digit"); // Array
+
+let firstVal = ""; // Valore iniziale
+let secondVal = ""; // Valore finale
+let operator =  null; // Operatore
